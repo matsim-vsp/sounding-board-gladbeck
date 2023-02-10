a271f36e03ef57332eaf46755ae99a7802bd6a6d
Friedrich Kasper Voelkers <friedrich.voelkers@me.com>
2023-02-10 13:47:30 +0100

    update plot scale

--- src/views/SoundingBoard.vue
+++ src/views/SoundingBoard.vue
@@ -170,7 +170,7 @@ export default class VueComponent extends Vue {
 
   private formattedValue(v: number) {
     const percent = 100 * (v - 1)
-    const sign = percent < 0 ? '' : '+'
+    const sign = percent <= 0 ? '' : '+'
     return sign + percent.toFixed(0)
   }
 
