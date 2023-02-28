939d74b32a31ab374c73e3b7fe54c3de40436f43
Friedrich Kasper Voelkers <friedrich.voelkers@me.com>
2023-02-28 10:16:47 +0100

    remove title for descriptionOutput and add another config file

--- src/components/TopNavBar.vue
+++ src/components/TopNavBar.vue
@@ -22,6 +22,7 @@ nav.navbar.is-link(role="navigation" aria-label="main navigation")
 
       .navbar-end
         router-link.navbar-item(:to="'/ccc/config'") privater Personenverkehr
+        router-link.navbar-item(:to="'/ccc/config_privaterPersonenverkehr'") privater Personenverkehr Kopie
         router-link.navbar-item(:to="'/ccc/config_gueter'") Güterverkehr
         router-link.navbar-item(:to="'/ccc/config_kommerziell'") kommerzieller Personenverkehr
         router-link.navbar-item(:to="'/ccc/config_sonder'" ) Sonderverkehr
