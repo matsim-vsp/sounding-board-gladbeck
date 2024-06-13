10c1af3c3c1fc26b3ef8dc55a19c0e2b3738511a
Billy Charlton <mail@billyc.cc>
2024-06-13 14:43:06 +0200

    allow votes from the ENTIRE INTERNET :-O

--- api-server/RunServer.py
+++ api-server/RunServer.py
@@ -51,7 +51,7 @@ valid_api_keys = setup_auth_keys(authfile)
 
 def is_valid_api_key():
     apikey = request.headers.get('Authorization')
-    if apikey in valid_api_keys: 
+    if apikey in valid_api_keys:
         print("-------VALID KEY---------")
         return True
     return False
@@ -84,18 +84,18 @@ valid_api_keys = setup_auth_keys(authfile)
 
 @app.route('/sessionOn', methods=["POST"])
 def turn_session_on():
-    if not is_valid_api_key(): 
+    if not is_valid_api_key():
         return "Invalid API Key", 403
-    
+
     data = request.get_data().decode("utf-8")
-    
+
     con = sqlite3.connect("test.db", check_same_thread=False)
-    cur = con.cursor()  
-    
+    cur = con.cursor()
+
     db_session_insert = "INSERT INTO sessions (sessionActive, sessionID, startTime, endTime) VALUES (?, ?, ?, ?)"
     db_session_end_insert = "UPDATE sessions SET endTime = ? WHERE sessionActive = 1"
     db_session_active_off_insert = "UPDATE sessions SET SessionActive = 0 WHERE sessionActive = 1"
-    
+
     start_time = ""
     end_time = ""
     session_active = None
@@ -107,39 +107,39 @@ def turn_session_on():
     if data == "true":
         start_time = current_time
         session_active = 1
-        
+
         # Get the current maximum sessionID from the table
         cur.execute("SELECT MAX(sessionID) FROM sessions")
         result = cur.fetchone()
         max_sessionID = result[0] if result[0] is not None else 0
-        
+
         # Calculate the next sessionID
         sessionID = max_sessionID + 1
-        
+
         cur.execute(db_session_insert, (session_active, sessionID, start_time, end_time))
-        
+
     elif data == "false":
         end_time = current_time
         cur.execute(db_session_end_insert, (end_time,))
         cur.execute(db_session_active_off_insert)
-        
+
     # Print the content of the 'sessions' table for debugging purposes
     table_list = [a for a in cur.execute("SELECT * FROM sessions ORDER BY ROWID ASC LIMIT 5")]
-    print(table_list) 
-    
+    print(table_list)
+
     con.commit()
     con.close()
-    
+
     return jsonify({"message": "instruction received successfully"}), 200, {"Access-Control-Allow-Origin": "*"}
 
 @app.route('/votes', methods=["POST"])
 def post_vote_to_db():
-    if not is_valid_api_key(): return "Invalid API Key", 403
+    # if not is_valid_api_key(): return "Invalid API Key", 403
     data = request.get_json()
-    
+
     con = sqlite3.connect("test.db", check_same_thread=False)
     cur = con.cursor()
-    
+
     cur.execute("SELECT sessionID FROM sessions WHERE EXISTS (SELECT sessionID FROM sessions WHERE sessionActive = 1)")
     result = cur.fetchone()
     if (result == None):
@@ -149,13 +149,13 @@ def post_vote_to_db():
         print("Active session")
         cur.execute("SELECT sessionID FROM sessions WHERE sessionActive = 1")
         sessionID = int(convertTuple(cur.fetchone()))
-        
+
     check_if_voter_exists = "SELECT * FROM votes WHERE cookie = ? AND  ipAddr = ?"
     cur.execute(check_if_voter_exists, (1, data['ipAddr']))
     voter_exists = cur.fetchone()
-    # if (voter_exists != None): 
-    #     update_vote = """Update votes 
-    #                 SET oepnv = ?, kiezBloecke = ?, fahrrad = ?, 
+    # if (voter_exists != None):
+    #     update_vote = """Update votes
+    #                 SET oepnv = ?, kiezBloecke = ?, fahrrad = ?,
     #                 parkraum = ?, fahrenderAutoVerkehr = ?, drt= ?, sessionID = ?
     #                 WHERE cookie = ? AND  ipAddr = ?"""
     #     cur.execute(update_vote, (
@@ -166,21 +166,21 @@ def post_vote_to_db():
     #         data['fahrenderAutoVerkehr'],
     #         data['drt'],
     #         sessionID,
-    #         1, 
+    #         1,
     #         data['ipAddr']))
-        
+
     #     table_list = [a for a in cur.execute("SELECT * FROM votes ORDER BY ROWID ASC LIMIT 37")]
-    #     print(table_list) 
+    #     print(table_list)
 
     #     con.commit()
     #     con.close()
-        
+
     #     return jsonify({"message": "Vote received successfully"}), 200, {"Access-Control-Allow-Origin": "*"}
 
     # else:
-    db_vote_insert = """INSERT INTO votes (oepnv, kiezBloecke, fahrrad, parkraum, fahrenderAutoVerkehr, drt, ipAddr, cookie, sessionID) 
+    db_vote_insert = """INSERT INTO votes (oepnv, kiezBloecke, fahrrad, parkraum, fahrenderAutoVerkehr, drt, ipAddr, cookie, sessionID)
                     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"""
-        
+
     cur.execute(db_vote_insert, (
     data['oepnv'],
     data['kiezBloecke'],
@@ -192,9 +192,9 @@ def post_vote_to_db():
     data['cookie'],
     sessionID
     ))
-    
+
     table_list = [a for a in cur.execute("SELECT * FROM votes ORDER BY ROWID ASC LIMIT 37")]
-    print(table_list) 
+    print(table_list)
 
     con.commit()
     con.close()
@@ -204,20 +204,20 @@ def post_vote_to_db():
 @app.route('/getVotes/<session_id>', methods=["GET"])
 def get_votes_from_db(session_id):
     if not is_valid_api_key(): return "Invalid API Key", 403
-    
+
     con = sqlite3.connect("test.db", check_same_thread=False)
     cur = con.cursor()
     cur.execute("SELECT * FROM votes WHERE sessionID = " + session_id)
     results = cur.fetchall()
     votes = [{'oepnv': row[0], 'kiezBloecke': row[1], 'fahrrad': row[2], 'parkraum': row[3], 'fahrenderAutoVerkehr': row[4], 'drt': row[5], 'ipAddr': row[6], 'cookie': row[7], 'session_id': row[8]} for row in results]
     print(votes)
-    
+
     return json.dumps(votes)
-    
+
 @app.route('/getLastSession/', methods=["GET"])
 def get_last_session():
     if not is_valid_api_key(): return "Invalid API Key", 403
-    
+
     con = sqlite3.connect("test.db", check_same_thread=False)
     cur = con.cursor()
     cur.execute("SELECT MAX(sessionID) FROM sessions")
@@ -226,6 +226,6 @@ def get_last_session():
         return "0"
     else:
         return result
-    
+
 if __name__ == "__main__":
     app.run(port=4999, debug=True)
